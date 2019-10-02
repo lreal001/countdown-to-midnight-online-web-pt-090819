593cb5f593d175308e88424fa8d3a4e987b9440f
Lessllie Real <rlessllie@yahoo.com>
2019-10-02 06:40:23 +0000

    Done.

--- countdown.rb
+++ countdown.rb
@@ -1,4 +1,18 @@
-#write your code here
 
-def countdown
-end
+
+def countdown(integer)
+   
+while integer > 0 
+puts "#{integer} SECOND(S)!"
+integer -= 1
+end 
+return "HAPPY NEW YEAR!"
+
+end 
+
+
+def countdown_with_sleep(string)
+  sleep(5)
+  
+end 
+
